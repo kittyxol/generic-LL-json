aff6f492b222ead0a027aaf974d2c274c9800af9
катечка <gerzod@Mac.Dlink>
2026-07-22 19:10:43 +0700

    add fixes MR resolve

---
+++ json.go
@@ -0,0 +1,145 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Node[T any] struct {
+	val  T
+	next *Node[T]
+}
+
+func (n *Node[T]) MarshalJSON() ([]byte, error) {
+	if n == nil {
+		return json.Marshal(nil)
+	}
+	return json.Marshal(struct {
+		Value T        `json:"value,omitempty"`
+		Next  *Node[T] `json:"child,omitempty"`
+	}{
+		Value: n.val,
+		Next:  n.next,
+	})
+}
+
+func (n *Node[T]) UnmarshalJSON(data []byte) error {
+	type nodeDTO struct { //type nodeDTO structure. Для JSON важны публичные поля, структура может быть приватной
+		Value T        `json:"value"`
+		Child *nodeDTO `json:"child"`
+	}
+	var i nodeDTO
+	if err := json.Unmarshal(data, &i); err != nil {
+		return err
+	}
+	n.val = i.Value
+	curN := n //корень
+	curDTO := &i //дто 
+	for curDTO.Child != nil { //
+		curN.next = &Node[T]{}
+		curN = curN.next
+		curDTO = curDTO.Child
+		curN.val = curDTO.Value
+	}
+	return nil
+}
+
+type LinkedList[T any] struct {
+	head *Node[T]
+}
+
+func (l *LinkedList[T]) Prepend(i T) {
+	l.head = &Node[T]{val: i, next: l.head}
+}
+
+func (l *LinkedList[T]) Append(i T) {
+	z := &Node[T]{val: i, next: nil}
+	if l.head == nil {
+		l.head = z
+		return
+	}
+	cur := l.head
+	for cur.next != nil {
+		cur = cur.next
+	}
+	cur.next = z
+
+}
+
+func (l *LinkedList[T]) UnmarshalJSON(data []byte) error {
+	type listDTO struct {
+		Head   *Node[T] `json:"head"`
+		Lenght int              `json:"lenght"`
+	}
+	var i listDTO
+	if err := json.Unmarshal(data, &i); err!=nil {
+		return err
+	}
+	if i.Head != nil {
+		l.head =i.Head 
+	}
+	return nil
+}
+
+func (l *LinkedList[T]) MarshalJSON() ([]byte, error) {
+	if l == nil {
+		return json.Marshal(nil)
+	}
+	Leng := l.Len()
+	return json.Marshal(struct {
+		Head   *Node[T] `json:"head,omitempty"`
+		Length int      `json:"length"`
+	}{
+		Head:   l.head,
+		Length: Leng,
+	})
+}
+func (l *LinkedList[T]) Len() int {
+	c := 0
+	cur := l.head
+	for cur != nil {
+		c++
+		cur = cur.next
+	}
+	return c
+}
+func (l *LinkedList[T]) Print() {
+	cur := l.head
+	for cur != nil {
+		fmt.Printf("%v ->", cur.val)
+		cur = cur.next
+	}
+	fmt.Println("nil")
+}
+
+func main() {
+	list := &LinkedList[string]{}
+	fmt.Println("Введите строку:")
+	scanner := bufio.NewReader(os.Stdin)
+	text, _ := scanner.ReadString('\n')
+	words := strings.Fields(text)
+	for i := range words {
+		list.Append(words[i])
+	}
+
+	data, err := json.MarshalIndent(list, "", " ")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var list2 LinkedList[string]
+	err2 := json.Unmarshal(data, &list2)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	list2.Print()
+	fmt.Println(string(data))
+	// list := LinkedList{...}
+	// b, _ := json.Marshal(list)
+	// var newList LinkedList
+	// json.Unmarshal(&b, &newList)
+	// fmt.Println(newList)
+
+}
--- json_test.go
+++ json_test.go
@@ -1,44 +1,59 @@
 package main
 
 import (
-	"testing"
-  	"github.com/stretchr/testify/require"
 	"encoding/json"
+	"github.com/stretchr/testify/require"
+	"testing"
 )
 
-func TestLLToJson(t *testing.T){
+func TestLLToJson(t *testing.T) {
 	t.Parallel()
-	t.Run("Marshal",func(t *testing.T){
+	t.Run("Marshal", func(t *testing.T) {
 		t.Parallel()
 		list1 := &LinkedList[string]{}
 		list1.Append("a")
 		list1.Append("b")
 		list1.Append("c")
-		str1 := `{"head": { "value": "a", "child": { "value": "b", "child": {  "value": "c"}}},"length": 3}`
 		data, err := json.Marshal(list1)
 
 		var eList LinkedList[string]
-		str0 := `{"length": 0}`
-		//str01 := `{}`
 		data0, err0 := json.Marshal(&eList)
 		require.NoError(t, err0)
-		require.JSONEq(t, str0, string(data0))
-		//require.JSONEq(t, str01, string(data0))//тк Length int `json:"length"`?? неправда
+		require.JSONEq(t, string(data0), `{"length": 0}` )
 		require.NoError(t, err)
-		require.JSONEq(t, str1, string(data))
+		require.JSONEq(t, string(data), 
+		`{
+		"head": {
+  		"value": "a",
+  		"child": {
+		"value": "b",
+   		"child": {
+		"value": "c"
+			}
+		}
+		},
+		"length": 3
+		}`)
 
 		var nList LinkedList[int]
-		num := `{"head": { "value": 11, "child": { "value": 22}},"length": 2}`
 		nList.Append(11)
 		nList.Append(22)
 		dataN, errn := json.Marshal(&nList)
 		require.NoError(t, errn)
-		require.JSONEq(t, num, string(dataN))
+		require.JSONEq(t, string(dataN), 
+		`{
+		"head": { 
+		"value": 11, 
+		"child": { 
+		"value": 22
+			}
+		},
+		"length": 2}`)
 	})
 }
-func TestJsonToLL(t *testing.T){
+func TestJsonToLL(t *testing.T) {
 	t.Parallel()
-	t.Run("Unmarshal",func(t *testing.T){
+	t.Run("Unmarshal", func(t *testing.T) {
 		t.Parallel()
 		str2 := `{"head": { "value": "a", "child": { "value": "b", "child": {  "value": "c"}}},"length": 3}`
 		var list2 LinkedList[string]
